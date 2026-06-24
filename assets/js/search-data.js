c425f744587ea420de719d63817119f241e4e4a1
Raymond112514 <122929649+Raymond112514@users.noreply.github.com>
2026-06-24 16:30:32 +0000

    Deploying to gh-pages from @ Raymond112514/raymond112514.github.io@61f7ac12c7df580958b587de5aa01707710ade93 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-courses",
+  },{id: "nav-courses",
           title: "Courses",
           description: "",
           section: "Navigation",
